08f0c36626402a4f194e55ac2cc2602a13a9f164
Alexis López Zubieta <azubieta@uci.cu>
2014-11-01 16:23:55 -0400

    Improve Icons view representation. By adjusting the items sizes and disposition, setting the selection mode to contiguous and enabling word warp..

--- src/FileManager/c++/Views/BasicFolderViewFactory.cpp
+++ src/FileManager/c++/Views/BasicFolderViewFactory.cpp
@@ -7,7 +7,9 @@
 
 #include "BasicFolderViewFactory.h"
 
+#include <Qt>
 #include <QListView>
+#include <QStyledItemDelegate>
 
 BasicFolderViewFactory::BasicFolderViewFactory() {
     mSupportedViews["icon"] = "Icons";
@@ -19,6 +21,25 @@ BasicFolderViewFactory::~BasicFolderViewFactory() {
 QAbstractItemView* BasicFolderViewFactory::GetView(QString name) {
     if (name == "icon") {
         QListView *view = new QListView();
+        
+        view->setViewMode(QListView::IconMode);
+        view->setSpacing(20);
+        view->setIconSize(QSize(48, 48));
+        
+        // TODO: Implement an item delegate to give an apropiated size to the
+        //  items.
+        view->setUniformItemSizes(true);
+        view->setWrapping(true);
+
+        view->setTextElideMode(Qt::ElideMiddle);
+        view->setWordWrap(true);
+        
+        view->setMovement(QListView::Static);
+        view->setResizeMode(QListView::Adjust);
+        view->setSelectionMode(QAbstractItemView::ContiguousSelection);
+        
+        view->setLayoutMode(QListView::Batched);
+        view->setBatchSize(10);
         return view;
     }
 }
